cbfe9508a41b1350b97f31d6f613285a19f485b6
benjamin <ebenjaminhd11@gmail.com>
2026-09-15 14:19:23 -0500

    chore: register models in admin

--- research/admin.py
+++ research/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from research.models import (
+    Topic,
+    Claim,
+    Document,
+    DocumentTopic,
+    Source,
+    Research,
+    ResearchDocument,
+    DocumentChunk
+)
 
-# Register your models here.
+admin.site.register(Topic)
+admin.site.register(Claim)
+admin.site.register(Document)
+admin.site.register(DocumentTopic)
+admin.site.register(Source)
+admin.site.register(Research)
+admin.site.register(ResearchDocument)
+admin.site.register(DocumentChunk)
