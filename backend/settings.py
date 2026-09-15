3fe701e4d6fa8d863d0ca47833210d2b21e7c474
benjamin <ebenjaminhd11@gmail.com>
2026-09-15 12:24:41 -0500

    chore: add basic API pagination and throttle config

--- backend/settings.py
+++ backend/settings.py
@@ -125,4 +125,13 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.AllowAny",
     ],
+    "PAGE_SIZE" : 50,
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "30/min",
+        "user": "120/min",
+    },
 }
